f67c32d3e294525157a4bfdcf9e816ea34be3fd4
DarkNightRequiem <13606030307@139.com>
2018-11-05 19:59:58 +0800

    新建bootlab工程

---
+++ src/main/java/name/dnr/bootlab/BootlabApplication.java
@@ -0,0 +1,12 @@
+package name.dnr.bootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootlabApplication.class, args);
+    }
+}

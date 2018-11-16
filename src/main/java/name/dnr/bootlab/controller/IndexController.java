f8433c72f66e92168010e03d8cf5cc9247c1df01
DarkNightRequiem <13606030307@139.com>
2018-11-16 20:12:26 +0800

    首页前端大致完成

--- src/main/java/name/dnr/bootlab/controller/IndexController.java
+++ src/main/java/name/dnr/bootlab/controller/IndexController.java
@@ -5,13 +5,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
     // 这种写法必须在pom.xml中加入依赖spring-boot-starter-thymeleaf
     // 否则会出现url和视图重名的错误
     @RequestMapping("/")
     public String hello(ModelMap map){
         map.addAttribute("name","Spring Boot");
-        return "hello2";
+        return "index";
     }
 }

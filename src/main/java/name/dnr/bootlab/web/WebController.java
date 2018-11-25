4655068e0e2afa67b49893f1a5fdb1279a653611
DarkNightRequiem <13606030307@139.com>
2018-11-25 21:00:34 +0800

    将sblearn请求从WebController换到IndexController中

--- src/main/java/name/dnr/bootlab/web/WebController.java
+++ src/main/java/name/dnr/bootlab/web/WebController.java
@@ -1,13 +1,30 @@
 package name.dnr.bootlab.web;
 
+import name.dnr.bootlab.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
 public class WebController {
 
-    @RequestMapping("/sblearn")
-    public String sbLearn(){
-        return "sblearn";
+
+
+    @RequestMapping("/saveUser")
+    public void saveUser(@Valid User user, BindingResult result){
+        // @Valid代表此对象使用了参数校验
+        // 参数校验结果存放在BidingResult中
+        System.out.println("user:"+user);
+        if(result.hasErrors()){
+            List<ObjectError> list=result.getAllErrors();
+            for(ObjectError error:list){
+                System.out.print(error.getCode()+"-"+error.getDefaultMessage());
+            }
+        }
     }
 }

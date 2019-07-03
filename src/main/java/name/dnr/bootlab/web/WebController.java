23b0482c6f7bb1338165e188d950e25e7d056217
wangyq1 <wangyq1@wangsu.com>
2019-07-03 13:55:07 +0800

    添加用get方式URL传参

--- src/main/java/name/dnr/bootlab/web/WebController.java
+++ src/main/java/name/dnr/bootlab/web/WebController.java
@@ -4,15 +4,25 @@ import name.dnr.bootlab.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
 public class WebController {
 
+    // 使用URL传参
+    @RequestMapping(value = "/get/{name}",method = RequestMethod.GET)
+    public String get(@PathVariable String name){
+        // GET方式请求的编码只支持ISO_8859_1，所以在这里做编码转换
+        name=new String(name.getBytes(StandardCharsets.ISO_8859_1),StandardCharsets.UTF_8);
+        return name;
+    }
 
 
     @RequestMapping("/saveUser")

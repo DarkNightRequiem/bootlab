8e065e97371bc3c39adced43f4e79d709fca91ea
wangyq1 <wangyq1@wangsu.com>
2019-08-12 18:25:23 +0800

    添加要注意的问题

--- src/main/java/name/dnr/bootlab/BootlabApplication.java
+++ src/main/java/name/dnr/bootlab/BootlabApplication.java
@@ -38,4 +38,26 @@ public class BootlabApplication {
         });
         return tomcat;
     }
+
+    /*@RequestParam
+用来处理Content-Type: 为 application/x-www-form-urlencoded编码的内容
+
+consumes="application/json"说明这个接口只处理Content-Type="application/json"的请求
+headers="Referer=http://www.csdn.com/*"表示request请求头中必须包含Referer=http://www.csdn.com/* produces = "application/json;charset=UTF-8"会与请求头中的Accept进行匹配，如果Accept中也包含application/json，这时才返回数据，如果Accept中没有定义application/json，则服务器端会报错：Could not find acceptable representation 当你有如下Accept头，将遵守如下规则进行应用：
+
+Spring Boot https
+
+Spring Boot 跨域   nginx反向代理实现跨域
+MyBatis接口注解
+
+微信小程序的生命周期 静默登录 公私token
+
+@ResultMap的服用和返回类型的问题，将先定义@Results的设置id，然后在别的地方复用。
+
+EsLint代码风格审查
+大数据需要使用 redis
+数据库模糊查询搜搜过慢优化，使用索引
+需求分析没有做好面向用户 市场调研 市场需求--运营 如何吸引算法用户过来
+数据库关联查询的效率问题
+@RequestParam("FF") @RequestBody*/
 }
